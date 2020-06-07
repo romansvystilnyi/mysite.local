ad3e1c107233120d9895c57d4811b6f6346e2e0d
Roman Svystilnyi <romansvystilny@gmail.com>
2020-06-07 15:16:00 +0300

    Fixed mistakes.

--- index.php
+++ index.php
@@ -11,60 +11,60 @@
     <link rel="stylesheet" href="css/style_footer.css">
 </head>
 <body>
-    <header class="site-header">
-        <!-- Верхняя часть страницы -->
-        <div class="container">
-            <div class="block-header-170">
-                <a href="http://mysite.local/">
-                    <img src="images/main_logo.png" alt="logo" class="main-logo">
-                </a>
+<header class="site-header">
+    <!-- Верхняя часть страницы -->
+    <div class="container">
+        <div class="block-header-170">
+            <a href="http://mysite.local/">
+                <img src="images/main_logo.png" alt="logo" class="main-logo">
+            </a>
+        </div>
+        <div class="block-header-1030">
+            <h1 class="hello">My site</h1>
+        </div>
+    </div>
+    <!-- Верхняя часть страницы -->
+</header>
+
+<div class="menu-wrap row">
+    <!-- Меню -->
+    <menu class="menu container">
+        <ul class="main-menu">
+            <li class="menu-item">
+                <a href="http://mysite.local/lessons/lesson_1">Урок 1</a>
+            </li>
+            <li class="menu-item">
+                <a href="http://mysite.local/lessons/lesson_2">Урок 2</a>
+            </li>
+            <li class="menu-item">
+                <a href="http://mysite.local/lessons/lesson_3">Урок 3</a>
+            </li>
+        </ul>
+    </menu>
+    <!-- Меню -->
+</div>
+
+<div class="site-main-wrap row">
+    <main class="main-content container">
+        <h1>“Hello, <b>NIX Education</b>”</h1>
+    </main>
+</div>
+
+<footer class="site-footer">
+    <!-- Нижняя часть страницы -->
+    <div class="container">
+        <div class="row">
+            <div class="block-footer-370">
+                <img src="images/footer_logo.png" alt="logo" class="footer-logo">
+                <p class="copyright">Copyright @2020</p>
             </div>
-            <div class="block-header-1030">
-                <h1 class="hello">My site</h1>
+            <div class="block-footer-770">
+                <p class="text-footer">My site</p>
             </div>
         </div>
-        <!-- Верхняя часть страницы -->
-    </header>
-
-    <div class="menu-wrap row">
-        <!-- Меню -->
-        <menu class="menu container">
-            <ul class="main-menu">
-                <li class="menu-item">
-                    <a href="http://mysite.local/lessons/lesson_1">Урок 1</a>
-                </li>
-                <li class="menu-item">
-                    <a href="http://mysite.local/lessons/lesson_2">Урок 2</a>
-                </li>
-                <li class="menu-item">
-                    <a href="http://mysite.local/lessons/lesson_3">Урок 3</a>
-                </li>
-            </ul>
-        </menu>
-        <!-- Меню -->
     </div>
-
-    <div class="site-main-wrap row">
-        <main class="main-content container">
-            <h1>“Hello, <b>NIX Education</b>”</h1>
-        </main>
-    </div>
-
-    <footer class="site-footer">
-        <!-- Нижняя часть страницы -->
-        <div class="container">
-            <div class="row">
-                <div class="block-footer-370">
-                    <img src="images/footer_logo.png" alt="logo" class="footer-logo">
-                    <p class="copyright">Copyright @2020</p>
-                </div>
-                <div class="block-footer-770">
-                    <p class="text-footer">My site</p>
-                </div>
-            </div>
-        </div>
-        <!-- Нижняя часть страницы -->
-    </footer>
+    <!-- Нижняя часть страницы -->
+</footer>
 
 </body>
 </html>
--- lessons/lesson_3/back.php
+++ lessons/lesson_3/back.php
@@ -49,13 +49,15 @@
         <p>
             <b>
                 <?php
-                    require_once "../../include/string_rev.php";
-                    echo string_rev($_POST['etext']);
+                require_once "../../include/string_rev.php";
+                echo string_rev($_POST['etext']);
                 ?>
             </b>
         </p>
         <br>
-        <a href="http://mysite.local/lessons/lesson_3"><button>Назад</button></a>
+        <a href="http://mysite.local/lessons/lesson_3">
+            <button>Назад</button>
+        </a>
     </main>
 </div>
 
