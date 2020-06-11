b723b27ceebb4113db3a8160d242002ffa0cffd1
Roman Svystilnyi <romansvystilny@gmail.com>
2020-06-11 18:10:51 +0300

    Post data is stored in a separate file as an associative array.

---
+++ pages/list_of_posts/array_posts.php
@@ -0,0 +1,7 @@
+<?php
+$posts = array(
+    "post_1" => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur id impedit laudantium maiores minima minus odit praesentium quos, ullam vero! Ad cumque, fugit officiis qui quod repellendus sit ullam voluptatum.',
+    "post_2" => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit, temporibus.',
+    "post_3" => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium amet architecto assumenda, beatae commodi deserunt ea esse et facilis ipsam ipsum maxime, molestiae officiis rerum similique unde vel voluptate, voluptatem voluptatum!',
+    "post_4" => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam beatae deserunt optio suscipit veniam?',
+);
--- pages/list_of_posts/list_of_post.php
+++ pages/list_of_posts/list_of_post.php
@@ -1,14 +1,35 @@
-<ul class="list-of-post">
-    <li>
-        <a href="#">Пост 1</a>
-    </li>
-    <li>
-        <a href="#">Пост 2</a>
-    </li>
-    <li>
-        <a href="#">Пост 3</a>
-    </li>
-    <li>
-        <a href="#">Пост 4</a>
-    </li>
-</ul>
+<?php
+require_once 'array_posts.php';
+?>
+<section id="posts">
+    <article>
+        <h2>Пост 1</h2>
+        <p>
+            <?php
+            print($posts['post_1']);
+            ?>
+        </p>
+    </article>
+    <article>
+        <h2>Пост 2</h2>
+        <p>
+            <?php
+            print($posts['post_2']);
+            ?>
+        </p>
+    </article>
+    <article>
+        <h2>Пост 3</h2>
+        <p>
+            <?php
+            print($posts['post_3']);
+            ?>
+        </p>
+    </article>
+    <article>
+        <h2>Пост 4</h2>
+        <?php
+        print($posts['post_4']);
+        ?>
+    </article>
+</section>
